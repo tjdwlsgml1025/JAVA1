a349b0979d13de905ba25906f6901af0c6180bb0
DESKTOP-RB9LR74\USER <gmlsid123@naver.com>
2023-07-13 15:43:45 +0900

    7/13

--- src/timer/Star3.java
+++ src/timer/Star3.java
@@ -1,15 +1,37 @@
 package timer;
 
+class GetSum{
+	int num;
+	
+	void setNum(int num1) {
+	num=num1;
+	}
+	
+	int sum() {
+		int sum=0;
+		for(int i=1; i<=num;i++) {
+			sum=sum+i;
+		}
+		return sum;
+	}
+}
+
 public class Star3 {
 
 	public static void main(String[] args) {
-		for(int i=0; i<5; i++) {
-			for(int j=1; j<=i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		GetSum getsum = new GetSum();
+        int num; //
+
+        num = 100;
+
+        getsum.setNum(num);
+
+        num = getsum.sum(); 
+        System.out.println(num);
+
+        getsum.setNum(1000);
+        num = getsum.sum();
+        System.out.println(num);
 
 	}
-
 }
---
+++ src/timer/Sum1_100.java
@@ -0,0 +1,17 @@
+package timer;
+
+public class Sum1_100 {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		for(int num = 1; num<100; num++) {
+			if(num%5 !=0 || num%7!=0)
+				continue;
+			count++;
+			System.out.println(num);
+		}
+		System.out.println(count);
+	}
+
+}
